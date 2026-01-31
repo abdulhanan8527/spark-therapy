ddfe6c49ee77dd7b1705721183740f7301598551
User <user@example.com>
2026-02-01 01:56:43 +0500

    Fix version mismatches and configure for Expo Go APK build

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Removed problematic plugins for Expo Go compatibility
+      "@babel/plugin-transform-optional-chaining",
     ],
   };
 };
